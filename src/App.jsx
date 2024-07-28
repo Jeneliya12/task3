7656403a682e422167fa8aaa676b1ea00ace551d
Jeneliya12 <gurungjeneliya@gmail.com>
2024-07-28 18:10:43 -0400

    Create React app with Create Vite App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hi, I am Jeneliya</h1>
+      <p>I am a Full Stack Engineer.</p>
+    </>
+  );
+}
+
+export default App;
